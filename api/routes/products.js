f984a91924cde81949ac77dc7283d3a28f91d4a5
jaguso <jagutzo@gmail.com>
2018-11-23 16:41:35 -0600

    delete product by ID works

--- api/routes/products.js
+++ api/routes/products.js
@@ -73,9 +73,18 @@ router.patch('/:productId', (req, res, next) => {
 
 
 router.delete('/:productId', (req, res, next) => {
-    res.status(200).json({
-        message: 'Deleted product!'
-    });
+    const id = req.params.productId;    
+    Product.remove({_id: id})
+        .exec()
+        .then(result => {
+            res.status(200).json(result);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            });
+        });
 });
 
 module.exports = router;
