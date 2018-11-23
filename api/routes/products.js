d4e9fbd852ce044a022a14240cdab818f3a74232
jaguso <jagutzo@gmail.com>
2018-11-23 16:26:22 -0600

    handling errors

--- api/routes/products.js
+++ api/routes/products.js
@@ -17,30 +17,42 @@ router.post('/', (req, res, next) => {
         name: req.body.name,
         price: req.body.price
     });
-    product.save().then(result => {
-        console.log(result);
+    product
+        .save()
+        .then(result => {
+            console.log(result);
+            res.status(201).json({
+                message: 'Handling POST request to /products',
+                createdProduct: product
+            });
     })
-    .catch(err => console.log(err));
-
-    res.status(201).json({
-        message: 'Handling POST request to /products',
-        createdProduct: product
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({
+            error: err
+        })
     });
+
 });
 
 // /:prid quiere decir un id en el url pero puede ser cualquier cosa (no necesariamente llamarse id)
 router.get('/:productId', (req, res, next) => {
     const id = req.params.productId; //aquí estamos llamando el id del url
-    if(id === 'special') {
-        res.status(200).json({
-            message: 'You discovered the special id',
-            id: id
+    Product.findById(id)
+        .exec()
+        .then(doc => {
+            console.log("From data base", doc);
+            if (doc) {
+                res.status(200).json(doc);
+            } else {
+                res.status(404).json({message: 'No valid entry found for provided ID'})
+            }
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({error: err});
         });
-    } else {
-        res.status(200).json({
-            message: 'You passed an id'
-        });
-    }
+
 });
 
 
