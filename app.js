eb7158fdf14f2a30227df8938b2684173b239ee6
jaguso <jagutzo@gmail.com>
2018-11-22 20:17:08 -0600

    added stuff to prevent cors errors

--- app.js
+++ app.js
@@ -10,6 +10,24 @@ app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended: false})); //para parsear otras cosas (si ponemos true, se extiende a cosas que no necesitamos ahora)
 app.use(bodyParser.json()); //es para parsear json
 
+//esto tiene que ir antes que las rutas
+//la segunda entrada de res.header dice a quien le damos acceso al api
+//obs req.method nos dice si es post, get, etc
+//obs estas restricciones solo sirven para pags web y no para postman
+app.use((req, res, next) => {
+    res.header('Access-Controll-Allow-Origin', '*');
+    res.header(
+        'Access-Controll-Allow-Headers', 
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    );
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).json({})
+    }
+    next();
+});
+
+
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
 
