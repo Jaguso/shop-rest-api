77af31bbc336926035da8c77b7615c16d59e06f1
jaguso <jagutzo@gmail.com>
2018-11-23 16:32:47 -0600

    get all api works

--- api/routes/products.js
+++ api/routes/products.js
@@ -5,9 +5,18 @@ const mongoose = require('mongoose');
 const Product = require('../models/products');
 
 router.get('/', (req, res, next) => {
-    res.status(200).json({
-        message: 'Handling GET request to /products'
-    });
+    Product.find()
+        .exec()
+        .then(docs => {
+            console.log(docs);
+            res.status(200).json(docs);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            });
+        });
 });
 
 //obs: en req.body usamos body-parser
