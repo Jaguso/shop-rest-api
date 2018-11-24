cce83c41d701b37b5e078aa383b1f3f71b091478
jaguso <jagutzo@gmail.com>
2018-11-23 19:05:47 -0600

    edited the server response for each request in products

--- api/routes/products.js
+++ api/routes/products.js
@@ -4,18 +4,28 @@ const mongoose = require('mongoose');
 
 const Product = require('../models/products');
 
+//.select() es para seleccionar que campos quiere que se creen
 router.get('/', (req, res, next) => {
     Product.find()
+        .select('name price _id')
         .exec()
         .then(docs => {
-            console.log(docs);
-            // if (docs.length >= 0) {
-            res.status(200).json(docs);
-            // } else {
-            //     res.status(404).json({
-            //         message: "No entries found"
-            //     });
-            // }
+            const response = {
+                count: docs.length,
+                products: docs.map(doc => {
+                    return {
+                        name: doc.name,
+                        price: doc.price,
+                        _id: doc._id,
+                        request: {
+                            type: 'GET',
+                            url: 'http://localhost:3000/products' + doc._id
+                        }
+                    }
+                })
+            }
+            console.log(response);
+            res.status(200).json(response);
         })
         .catch(err => {
             console.log(err);
@@ -37,8 +47,16 @@ router.post('/', (req, res, next) => {
         .then(result => {
             console.log(result);
             res.status(201).json({
-                message: 'Handling POST request to /products',
-                createdProduct: product
+                message: 'Created product successfully',
+                createdProduct: {
+                    name: result.name,
+                    price: result.price,
+                    _id: result._id,
+                    request: {
+                        type: 'GET',
+                        url: 'http://localhost:3000/products' + result._id
+                    }
+                }
             });
     })
     .catch(err => {
@@ -50,15 +68,23 @@ router.post('/', (req, res, next) => {
 
 });
 
-// /:prid quiere decir un id en el url pero puede ser cualquier cosa (no necesariamente llamarse id)
+// /:productId quiere decir un id en el url y puede tener cualquier nombre (en este caso 'productId')
 router.get('/:productId', (req, res, next) => {
     const id = req.params.productId; //aquí estamos llamando el id del url
     Product.findById(id)
+        .select('name price _id')
         .exec()
         .then(doc => {
             console.log("From data base", doc);
             if (doc) {
-                res.status(200).json(doc);
+                res.status(200).json({
+                    product: doc,
+                    request: {
+                        type: 'GET',
+                        description: 'Get all products',
+                        url: 'http://localhost:3000/products'
+                    }
+                });
             } else {
                 res.status(404).json({message: 'No valid entry found for provided ID'})
             }
@@ -81,7 +107,13 @@ router.patch('/:productId', (req, res, next) => {
         .exec()
         .then(result => {
             console.log(result);
-            res.status(200).json(result);
+            res.status(200).json({
+                message: 'Product updated',
+                request: {
+                    type: 'GET',
+                    url: 'http://localhost:3000/products/' + id
+                }
+            });
         })
         .catch(err => {
             console.log(err);
@@ -97,7 +129,14 @@ router.delete('/:productId', (req, res, next) => {
     Product.remove({_id: id})
         .exec()
         .then(result => {
-            res.status(200).json(result);
+            res.status(200).json({
+                message: 'Product deleted',
+                request: {
+                    type: 'POST',
+                    url: 'http://localhost:3000/products',
+                    body: {name: 'String', price: 'Number'}
+                }
+            });
         })
         .catch(err => {
             console.log(err);
--- app.js
+++ app.js
@@ -16,6 +16,7 @@ mongoose.connect(
         useNewUrlParser: true
     }
 );
+mongoose.Promise = global.Promise;
 
 
 app.use(morgan('dev'));
