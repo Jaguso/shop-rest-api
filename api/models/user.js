291d0aca0430caf4ca66d37be55ae30274a4c921
jaguso <jagutzo@gmail.com>
2018-11-27 18:19:48 -0600

    previus commit was signup. Delete users works and email validation

--- api/models/user.js
+++ api/models/user.js
@@ -2,9 +2,16 @@ const mongoose = require('mongoose');
 
 const userSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
-    email: { type: String, required: true },
+    email: { 
+        type: String, 
+        required: true, 
+        unique: true, 
+        match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
+    },
     password: { type: String, required: true }
 });
+
+//en match pusimos una regex que valide que lo que pongamos es un email
 
 module.exports = mongoose.model('User', userSchema);
 
